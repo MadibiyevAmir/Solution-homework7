031dcfd120fb4a60c9c24d14699ab524615051fc
MadibiyevAmir <amir.madibiyev@narxoz.kz>
2025-04-27 22:15:50 +0500

    CargoPlane.java

---
+++ binge-airport-simulator/src/airport/CargoPlane.java
@@ -0,0 +1,12 @@
+package airport;
+
+public class CargoPlane extends Aircraft {
+    public CargoPlane(String id, int fuelLevel) {
+        super(id, fuelLevel);
+    }
+
+    @Override
+    public void receive(String msg) {
+        System.out.println("[CargoPlane " + id + "] received: " + msg);
+    }
+}
